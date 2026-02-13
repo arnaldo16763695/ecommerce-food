a87cb6545a6986b8a202a37c4e0b08adb0969732
Arnaldo Espinoza <arnaldoespinoza1@hotmail.com>
2026-02-13 15:11:55 -0400

    Set Turbopack root to avoid multi-lockfile warning

---
+++ lib/header-utils.ts
@@ -0,0 +1,5 @@
+export const getMenuButtonLabel = (openMenu: boolean) =>
+  openMenu ? "Close menu" : "Open menu";
+
+export const getMobileMenuVisibilityClass = (openMenu: boolean) =>
+  openMenu ? "visible grid" : "invisible hidden";
