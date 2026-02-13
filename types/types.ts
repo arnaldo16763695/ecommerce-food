bdfed9c811b6f1deba0ab39f641bf4e2a6149b18
Arnaldo Espinoza <arnaldoespinoza1@hotmail.com>
2026-02-13 19:59:25 -0400

    add types

---
+++ types/types.ts
@@ -0,0 +1,25 @@
+export type LoginCardProps = {
+  open: boolean;
+  setOpen: (value: boolean) => void;
+};
+export type productCardProps = {
+  id: number;
+  name: string;
+  price: number;
+  img: string;
+  star: any;
+};
+
+export type CartItem = {
+  id: number;
+  quantity: number;
+};
+export type CartStore = {
+  items: CartItem[];
+  addItem: (productId: number, quantity?: number) => void;
+  removeItem: (productId: number) => void;
+  updateQuantity: (productId: number, quantity: number) => void;
+  clearCart: () => void;
+};
+
+export type SortOption = "Sort By" | "Price:Low to Hight" | "name:A to Z";
