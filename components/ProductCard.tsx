2f9a20aba458bbecee20889797bce805ad2e748a
Arnaldo Espinoza <arnaldoespinoza1@hotmail.com>
2026-02-18 07:38:44 -0400

    Improve images in ProductCard component

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -16,7 +16,13 @@ function ProductCard({ id, img, price, name, star }: productCardProps) {
   return (
     <div className="group relative flex flex-col gap-2.5 rounded-md border border-slate-200 bg-white p-8 transition-all hover:-translate-y-1 hover:shadow-lg dark:border-slate-700 dark:bg-slate-800 dark:hover:shadow-black/30">
       <div className="relative flex h-full items-center justify-center rounded-xl bg-amber-50 py-10 dark:bg-slate-700/60">
-        <Image src={img} alt={name} width={200} height={200} />
+        <Image
+          src={img}
+          alt={name}
+          width={200}
+          height={200}
+          className="w-auto h-auto object-contain"
+        />
         <Link
           href={`/shop/product/${id}/details`}
           className="absolute right-0 top-0 gap-2 rounded-xs border border-amber-200 bg-white p-2 opacity-0 transition-all hover:text-amber-600 focus:text-amber-600 group-hover:opacity-100 dark:border-amber-300/40 dark:bg-slate-800 dark:text-slate-200"
